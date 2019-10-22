d8299971a4a7c9f8710b032263de2cc6ebe9af9e
VK <vitaliy.krynytskyy@gmail.com>
2019-10-22 14:36:48 +0100

    Refactored code

--- src/api/story.js
+++ src/api/story.js
@@ -1,4 +1,4 @@
-const HACKER_NEWS_API_URL = 'http://hn.algolia.com/api/v1/search?query=';
+const HACKER_NEWS_API_URL = 'https://hn.algolia.com/api/v1/search?query=';
 
 const fetchStories = query => fetch(`${HACKER_NEWS_API_URL}${query}`).then(response =>
     response.json()
